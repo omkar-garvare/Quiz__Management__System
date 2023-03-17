04348a8dfb6251a59c5ffbd1e2522e0377d8811e
Omkar Garvare <127541450+omkar-garvare@users.noreply.github.com>
2023-03-18 01:00:43 +0530

    Update allQuestion.java

--- src/allQuestion.java
+++ src/allQuestion.java
@@ -26,7 +26,7 @@ public class allQuestion extends javax.swing.JFrame {
 
     
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
